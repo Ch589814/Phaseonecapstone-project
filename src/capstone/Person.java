a0bfef48214a6c330f22987e81157e576579f817
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-25 14:18:46 +0200

    add person class with name and email

--- src/capstone/Person.java
+++ src/capstone/Person.java
@@ -1,4 +1,22 @@
 package capstone;
+public  abstract class Person {
+    private String name;
+        private String email;
 
-public class Person {
-}
+        public Person(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        // Getters & Setters
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public String getEmail() { return email; }
+        public void setEmail(String email) { this.email = email; }
+
+        // Abstract method to be implemented by subclasses
+        public abstract void displayInfo();
+    }
+
+
