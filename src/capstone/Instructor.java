db0b19ab06dcdd04c0c1123d215e5222ea8fdf55
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-25 14:25:19 +0200

    instructor file with id and departmet

--- src/capstone/Instructor.java
+++ src/capstone/Instructor.java
@@ -1,4 +1,24 @@
 package capstone;
+public class Instructor extends Person {
+    private String id;
+    private String dept;
 
-public class Instructor {
+    public Instructor(String name, String email, String instructorID, String department) {
+        super(name, email);
+        this.id =instructorID;
+        this.dept = department;
+    }
+
+    // Getters & Setters
+    public String getInstructorID() { return id; }
+    public void setInstructorID(String instructorID) { this.id = instructorID; }
+
+    public String getDepartment() { return dept; }
+    public void setDepartment(String department) { this.dept = department; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Instructor: " + getName() + " ( instructor ID: " + id + ")");
+        System.out.println("Dept: " + dept);
+    }
 }
